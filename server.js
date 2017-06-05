29b4b33cdf62fc4b1f2981afea3c0cfacb883204
steveraaen <sraaen@gmail.com>
2017-06-05 12:57:37 -0400

    moved all routes to within mongodb connection for bubbling

--- server.js
+++ server.js
@@ -23,6 +23,56 @@ db.on("error", function(error) {
 });
 db.once("open", function() {
     console.log("Mongoose connection successful.");
+
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/fails/:term", function(req, res) {
+    Fail.find({ "SYMBOL": req.params.term }, function(error, doc) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.send(doc);
+        }
+    });
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+});
+module.exports = app;
+
+
+
+
+/*var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var pwds = require('./pwds');
+var Fail = require('./models/Fails.js');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("./public"));
+mongoose.connect(pwds.mong);
+mongoose.Promise = Promise;
+
+var db = mongoose.connection;
+
+db.on("error", function(error) {
+    console.log("Mongoose Error: ", error);
+});
+db.once("open", function() {
+    console.log("Mongoose connection successful.");
 });
 
 app.get("/", function(req, res) {
@@ -43,3 +93,4 @@ app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
 module.exports = app;
+*/
