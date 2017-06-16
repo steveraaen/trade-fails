2b63e87a4ec97b84994d72e0f4cbeb15a0516121
steveraaen <sraaen@gmail.com>
2017-06-16 14:05:40 -0400

    removed polluted code

--- app/components/children/Results.js
+++ app/components/children/Results.js
@@ -1,37 +1,23 @@
 import React from "react";
 
 class Results extends React.Component {
-
   constructor(props) {
       super(props);
 
   }
-
-  componentDidUpdate() {
-
+componentDidUpdate() {
+    console.log('componentDidUpdate')
  
   }
-
-
   render() {
-    
-    return (
 
+    return (
       <div className="panel panel-default">
         <div className="panel-heading">
         <h1>Fails</h1>
           <h3 className="panel-title text-center">Results</h3>
         </div>
         <div className="panel-body text-center">
-          <ul>
-            
-            { JSON.stringify(this.props.results.data).map(function(search, i) {
-            return (
-              <p key={i}>{search._id} - {search.SYMBOL}</p>
-            );
-          })}
-          </ul>
-
 
         </div>
       </div>
